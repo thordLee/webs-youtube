cd3f2aae2d11db9b868092f213c6840b7274c471
thordLee <turtle0907@naver.com>
2024-01-02 04:57:37 +0900

    페이지SEO작업

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,14 @@
 import React from 'react'
 import Main from '../components/section/Main'
 
-
-
 const Home = () => {
-  return (
-    <div>
-      <Main>
-		Home
-	  </Main>
-    </div>
-  )
+    return (
+        <Main 
+            title = "웹스토리보이 유튜브"
+            description="웹스토리보이 유튜버 사이트에 오신 것을 환영합니다.">
+            Home
+        </Main>
+    )
 }
 
 export default Home
