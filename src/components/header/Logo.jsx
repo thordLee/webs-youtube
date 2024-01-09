25b10cc49209f189116c3cff835c941cd74a6a7a
thordLee <turtle0907@naver.com>
2024-01-10 06:56:16 +0900

    Rapid API 작업 및 더보기

--- src/components/header/Logo.jsx
+++ src/components/header/Logo.jsx
@@ -4,7 +4,7 @@ const Logo = () => {
   return (
     <h1 className="header__logo">
         <a href='/'>
-          <em aria-hidden='treu'> </em>
+          <em aria-hidden='true'> </em>
           <span>webs<br />youtube</span>
         </a>
       </h1>
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -3,12 +3,24 @@ import Main from '../components/section/Main'
 import VideoSearch from '../components/videos/VideoSearch'
 import { useParams } from 'react-router-dom'
 
+import { fetchFromAPI } from '../utils/api'
+
 const Search = () => {
 
     const { searchID } = useParams();
     const [ videos, setVideos ] = useState([]);
+    const [nextPageToken, setNextPageToken] = useState(null);
+
+    const [loading, setLoading] = useState(true);
+
+
     console.log(searchID);
     useEffect(()=>{
+        setVideos([]);
+        fetchVideos(searchID);
+        setLoading(false);
+
+        /*
         fetch(
             `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=30&q=${searchID}&key=${process.env.REACT_APP_YOUTUBE_API_KEY}`
             )
@@ -18,17 +30,42 @@ const Search = () => {
                 setVideos(result.items);
             })
             .catch(error => console.log(error))
+
+            */
     },[searchID]);
+
+    const fetchVideos = (query, pageToken='')=>{
+        fetchFromAPI(`search?part=snippet&q=${query}&pageToken=${pageToken}`)
+            .then((data)=>{
+                setNextPageToken(data.nextPageToken);
+                setVideos((preVideo)=>[...preVideo, ...data.items]);
+            })
+            .catch((error) => {
+                console.log('Error fetching data', error);
+            })
+    }
+
+    const handleLoadMore = () => {
+        if (nextPageToken) {
+            fetchVideos(searchID, nextPageToken);
+        }
+    }
+
+    const searchPageClass = loading?'isLoading':'isLoaded';
 
     return (
         <Main 
             title = "유투브 검색"
             description="유튜브 검색 결과 페이지입니다.">
-            <section id='searchPage'>
+            <section id='searchPage' className={searchPageClass}>
                 <div className="video__inner search">
                     <VideoSearch videos={videos}/>
                 </div>
-
+                <div className="video__more">
+                    {nextPageToken && (
+                        <button onClick={handleLoadMore}>더보기</button>
+                    )}
+                </div>
             </section>
         </Main>
     )
